6edeee1d107e0fcfd9c19a13162ede9cc134841a
irolaa <irolaluis20@gmail.com>
2025-07-02 23:29:37 -0600

    config

--- src/pages/history/types.ts
+++ src/pages/history/types.ts
@@ -1,11 +1,9 @@
-import { Question } from '../quiz/types';
 
 export type QuizHistoryItem = {
   id: string;
   date: string;
   category: string;
   score: string;
-  questions: Question[];
   answers: {
     [questionId: string]: string[];
   };
